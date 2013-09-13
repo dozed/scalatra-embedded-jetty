3b6e71e828ca358386f802eee8db4a266c97444b
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2013-09-13 22:27:39 +0200

    Added json test.

--- project/Build.scala
+++ project/Build.scala
@@ -26,6 +26,8 @@ object EmbeddedJettyBuild extends Build {
       "org.scalatra" %% "scalatra" % ScalatraVersion,
       "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
       "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+      "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+      "org.json4s"   %% "json4s-jackson" % "3.2.4",
       "ch.qos.logback" % "logback-classic" % "1.0.13",
       "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
       "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
--- src/main/scala/org/scalatra/embedded/EmbeddedJetty.scala
+++ src/main/scala/org/scalatra/embedded/EmbeddedJetty.scala
@@ -1,8 +1,25 @@
 package org.scalatra.embedded
 
 import org.scalatra.ApiFormats
+import org.scalatra.json.{JValueResult, JacksonJsonSupport}
+import org.json4s.DefaultFormats
 
-class EmbeddedJetty extends EmbeddedJettyStack {
+class EmbeddedJetty extends EmbeddedJettyStack with ApiFormats with JacksonJsonSupport {
+
+  implicit val jsonFormats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
+
+  get("/say-html") {
+    contentType = formats("html")
+    jade("index")
+  }
+
+  get("/say-json") {
+    "hellooo"
+  }
 
   get("/?") {
     f"Hello, from action! (isDevelopment = ${isDevelopmentMode}, serverHost = ${serverHost}, forceSsl = ${needsHttps}})"
